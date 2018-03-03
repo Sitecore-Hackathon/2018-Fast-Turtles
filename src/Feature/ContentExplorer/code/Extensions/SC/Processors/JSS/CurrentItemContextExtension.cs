8add5eaaa858937ed834e0c27ed109ad64a584a6
Artsem Prashkovich <a.prashkovich@brimit.com>
2018-03-03 11:28:32 +0300

    solved issue with config file

--- src/Feature/ContentExplorer/code/Extensions/SC/Processors/JSS/CurrentItemContextExtension.cs
+++ src/Feature/ContentExplorer/code/Extensions/SC/Processors/JSS/CurrentItemContextExtension.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Linq;
+using ContentExplorer.Model.JSS;
 using Sitecore.Data;
 using Sitecore.Data.Items;
 using Sitecore.LayoutService.ItemRendering.Pipelines.GetLayoutServiceContext;
 using Sitecore.Links;
+using Sitecore.Workflows;
 
 namespace ContentExplorer.Extensions.SC.Processors.JSS
 {
     public class CurrentItemContextExtension : Sitecore.JavaScriptServices.ViewEngine.LayoutService.Pipelines.GetLayoutServiceContext.JssGetLayoutServiceContextProcessor
     {
-        public const string CurrentItemObjectKey = "footerNavigation";
+        public const string CurrentItemObjectKey = "currentItem";
 
         public CurrentItemContextExtension(Sitecore.JavaScriptServices.Configuration.IConfigurationResolver configurationResolver) : base(configurationResolver)
         {
@@ -24,7 +26,20 @@ namespace ContentExplorer.Extensions.SC.Processors.JSS
             if (renderedItem == null || database == null)
                 return;
 
-            args.ContextData.Add(CurrentItemObjectKey, "works");
+            var outputObject = new CurrentItemModel();
+            outputObject.CreatedBy = renderedItem.Statistics.CreatedBy;
+            outputObject.UpdatedBy = renderedItem.Statistics.UpdatedBy;
+            outputObject.CountOfVersions = renderedItem.Versions.Count;
+            outputObject.CurrentVersion = renderedItem.Version.Number;
+
+            IWorkflow workflow = database.WorkflowProvider.GetWorkflow(renderedItem);
+            WorkflowState state = workflow.GetState(renderedItem);
+
+            outputObject.WorkflowState = state.DisplayName;
+
+            outputObject.IsPublished = !renderedItem.Publishing.NeverPublish;
+            
+            args.ContextData.Add(CurrentItemObjectKey, outputObject);
         }
     }
 }
--- src/Feature/ContentExplorer/code/Model/JSS/CurrentItemModel.cs
+++ src/Feature/ContentExplorer/code/Model/JSS/CurrentItemModel.cs
@@ -3,7 +3,11 @@
     public class CurrentItemModel
     {
         public Sitecore.Data.Fields.ImageField Icon { get; set; }
-        public string Creator { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
         public bool IsPublished { get; set; }
+        public string WorkflowState { get; set; }
+        public int CountOfVersions { get; set; }
+        public int CurrentVersion { get; set; }
     }
 }
