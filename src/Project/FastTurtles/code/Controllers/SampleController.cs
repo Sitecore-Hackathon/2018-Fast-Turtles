967dc3fa8cf7cc8e7467b0393436e158f86c29bc
Alexey Smolyanko <a.smolyanko@brimit.com>
2018-03-03 21:41:02 +0300

    add create contact

--- src/Project/FastTurtles/code/Controllers/SampleController.cs
+++ src/Project/FastTurtles/code/Controllers/SampleController.cs
@@ -1,12 +1,32 @@
-﻿using System.Web.Mvc;
-using Sitecore.Analytics;
-using Sitecore.Mvc.Controllers;
-using Sitecore.Web;
-
-namespace FastTurtles
+﻿namespace FastTurtles
 {
+    using System.Web.Mvc;
+
+    using FastTurtles.Model;
+
+    using Sitecore.Analytics;
+    using Sitecore.Mvc.Controllers;
+
     public class SampleController : SitecoreController
     {
+        public static void IdentifyUser(string username)
+        {
+            // Never identify an anonymous user
+            if (username.ToLower() == "extranet\\anonymous")
+            {
+                return;
+            }
+
+            var identificationSource = "website";
+            if (Tracker.Current != null && Tracker.Current.IsActive && Tracker.Current.Session != null)
+            {
+                Tracker.Current.Session.IdentifyAs(identificationSource, username);
+            }
+        }
+
+        [HttpPost]
+        public void AddContactUser(ContactViewModel viewModel) { }
+
         // GET
         public ActionResult IdentifyContact(string email)
         {
@@ -14,20 +34,8 @@ namespace FastTurtles
             {
                 IdentifyUser(email);
             }
-            return View();
-        }
 
-        public static void IdentifyUser(string username)
-        {
-            // Never identify an anonymous user
-            if (username.ToLower() == "extranet\\anonymous")
-                return;
-
-            string identificationSource = "website";
-            if (Tracker.Current != null && Tracker.Current.IsActive && Tracker.Current.Session != null)
-            {
-                Tracker.Current.Session.IdentifyAs(identificationSource, username);
-            }
+            return this.View();
         }
     }
 }
---
+++ src/Project/FastTurtles/code/Model/ContactViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FastTurtles.Model
+{
+    public class ContactViewModel
+    {
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
